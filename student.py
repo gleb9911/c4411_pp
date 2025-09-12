7f6da598df57376e79b8dce47a86731041ebb8c8
gleb9911 <gleb.starodubcev2011@gmail.com>
2025-09-12 21:58:34 +0300

    test

--- student.py
+++ student.py
@@ -1,63 +1,72 @@
 import random
 
 class Student:
-    def __init__(self,name ):
-        self.name=name
+    def __init__(self, name):
+        self.name = name
         self.gladness = 50
         self.progress = 0
+        self.money = 20
         self.alive = True
 
     def to_study(self):
-        print("time to study")
-        self.progress += 0.12
+        print(" Час вчитися")
+        self.progress += 0.2
         self.gladness -= 3
 
     def to_sleep(self):
-        print("time to sleep")
+        print("💤 Час спати")
         self.gladness += 3
 
-
     def to_chill(self):
-        print("rest time!")
+        print("Відпочинок")
         self.gladness += 5
         self.progress -= 0.1
+        self.money -= 2  # під час відпочинку витрачає гроші
+
+    def to_work(self):
+        print(" Працюю")
+        self.money += 10
+        self.gladness -= 2
+
     def is_alive(self):
         if self.progress < -0.5:
-            print("cast out...")
+            print("Відрахували...")
             self.alive = False
         elif self.gladness <= 0:
-            print("depression..")
+            print("Депресія...")
             self.alive = False
         elif self.progress >= 5:
-            print("passed externally")
+            print("Склав екстерном!")
             self.alive = False
 
+    def end_of_day(self):
+        print(f"Радість = {self.gladness}")
+        print(f"Успішність = {round(self.progress, 2)}")
+        print(f"Гроші = {self.money}")
 
-def end_of_day(self):
-    print(f"Gladness = {self.gladness}")
-    print(f"Progress = {round(self.progress, 2)}")
+    def live(self, day):
+        day_str = f"Day {day} of {self.name}'s life"
+        print(f"{day_str:=^50}")
 
+        cube = random.randint(1, 4)
+        if cube == 1:
+            self.to_study()
+        elif cube == 2:
+            self.to_sleep()
+        elif cube == 3:
+            self.to_chill()
+        elif cube == 4:
+            self.to_work()
 
-def live(self, day):
-    day = "Day" + str(day) + "of" + self.name + "life"
-    print(f"{day:=^50}")
-    live_cube = random.randint(1, 3)
-    if live_cube == 1:
-        self.to_study()
-    elif live_cube == 2:
-        self.to_sleep()
-    elif live_cube == 3:
-        self.to_chill()
-    self.end_of_day()
-    self.is_alive()
-  nick = Student(name="Nick")
-for day in range(365):
-     if nick.alive == False:
-           break
-       nick.live(day)
+        self.end_of_day()
+        self.is_alive()
 
 
-
+nick = Student(name="Nick")
+for day in range(1, 366):
+    if not nick.alive:
+        break
+    nick.live(day)
 
 
 
