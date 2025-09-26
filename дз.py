fc24b53c548a5f0d0ad9b4c0484bb6b15e6ee38d
gleb9911 <gleb.starodubcev2011@gmail.com>
2025-09-26 22:16:20 +0300

    INTRO SYS

---
+++ дз.py
@@ -0,0 +1,7 @@
+from colorama import init, Fore, Back, Style
+
+init()
+
+print(Fore.RED + "Червоний " + Style.RESET_ALL)
+print(Back.GREEN + "Зелений " + Style.RESET_ALL)
+print(Style.BRIGHT + "Яскравий " + Style.RESET_ALL)
